6d96fc705ec4dd55db73f44ae5debb528bb6d327
Oracy Martos <oramartos_21@hotmail.com>
2019-10-24 10:57:17 -0300

    removeAccent done

--- mining-utils.ts
+++ mining-utils.ts
@@ -1,8 +1,1 @@
-import { stringsManipulation } from './strings/index'
-
-stringsManipulation.removeAccent('Mussum Ipsum, cacilds vidis litro abertis. Em pé sem cair, deitado sem dormir, sentado sem cochilar e fazendo pose. Suco de cevadiss deixa as pessoas mais interessantis. Todo mundo vê os porris que eu tomo, mas ninguém vê os tombis que eu levo! Per aumento de cachacis, eu reclamis. ')
-
-// Mussum Ipsum, cacilds vidis litro abertis.Em pé sem cair, deitado sem dormir, sentado sem cochilar e fazendo pose.Suco de cevadiss deixa as pessoas mais interessantis.Todo mundo vê os porris que eu tomo, mas ninguém vê os tombis que eu levo! Per aumento de cachacis, eu reclamis.
-
-  // ['áÁàÀãÃâÂäÄ', 'éÉèÈẽẼêÊëË', 'íÍìÌĩĨîÎ', 'óÓòÒõÕÔ', 'úÚùÙũŨÛ', 'çÇĉĈ', 'ǵǴĝĜ', 'ĥĤḧḦ'
-  // , 'ĵĴ', 'ḱḰ', 'ĺĹ', 'ḿḾ', 'ńŃǹǸñÑ', 'ṕṔ', 'ŕŔ', 'śŚŝŜ', 'ẗ', 'ǘǗǜǛṽṼ', 'ẃẂẁẀŵŴẅẄ', 'ẍẌ', 'ýÝỳỲŷŶỹỸ', 'źŹẑẐ']
+import * as stringChanges from './strings/index'
--- strings/accent.ts
+++ strings/accent.ts
@@ -1,24 +1,37 @@
-export const removeAccent = (phrase?: string, words?: string[]): string[] | string  => {
-  if (phrase){
+const removeAccent = (phrase: string | string[]): string[] | string  => {
+  try {
     const replace: string[] = ['a', 'e', 'i', 'o', 'u', 'c', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'r', 's', 't', 'u', 'w', 'x', 'y', 'z']
     const accent: string[] = ['áÁàÀãÃâÂäÄ', 'éÉèÈẽẼêÊëË', 'íÍìÌĩĨîÎ', 'óÓòÒõÕÔ', 'úÚùÙũŨÛ', 'çÇĉĈ', 'ǵǴĝĜ', 'ĥĤḧḦ'
-                              , 'ĵĴ', 'ḱḰ', 'ĺĹ', 'ḿḾ', 'ńŃǹǸñÑ', 'ṕṔ', 'ŕŔ', 'śŚŝŜ', 'ẗ', 'ǘǗǜǛṽṼ', 'ẃẂẁẀŵŴẅẄ', 'ẍẌ', 'ýÝỳỲŷŶỹỸ', 'źŹẑẐ']
-    let newString: string
+      , 'ĵĴ', 'ḱḰ', 'ĺĹ', 'ḿḾ', 'ńŃǹǸñÑ', 'ṕṔ', 'ŕŔ', 'śŚŝŜ', 'ẗ', 'ǘǗǜǛṽṼ', 'ẃẂẁẀŵŴẅẄ', 'ẍẌ', 'ýÝỳỲŷŶỹỸ', 'źŹẑẐ']
 
-    for (const word of accent) {
-      if (phrase.match(`[${word}]`)) {
-        accent.indexOf(word)
-        newString = phrase.replace(`[${word}]`, replace[accent.indexOf(word)])
-        console.log(newString)
-        console.log(replace[accent.indexOf(word)])
-      } else{
-        // console.log(word)
+    let reg: RegExp
+    let data: string
+
+    if (!Array.isArray(phrase)) {
+      data = phrase.toString()
+      for (const word of accent) {
+        if (data.match(`[${word}]`)) {
+          reg = new RegExp(`[${accent[accent.indexOf(word)]}]`, "g")
+          data = data.replace(reg, replace[accent.indexOf(word)])
+        }
       }
-    }
-    // const newStr: string = phrase.replace(replace, "")
-    return 'a'
-  }else{
+      return data
 
-    return 'a'
+    } else {
+      for (let x = 0; x < phrase.length; x++) {
+        for (const word of accent) {
+          if (phrase[x].match(`[${word}]`)) {
+            reg = new RegExp(`[${accent[accent.indexOf(word)]}]`, "g")
+            phrase[x] = phrase[x].replace(reg, replace[accent.indexOf(word)])
+          }
+        }
+      }
+      return phrase
+
+    }
+  } catch (err) {
+    throw err
   }
 }
+
+export const accent = removeAccent
