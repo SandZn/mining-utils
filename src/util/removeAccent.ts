f3f6df6232da69489e391f3d902e6632d9303a5b
Oracy Martos <oramartos_21@hotmail.com>
2019-10-25 11:09:03 -0300

    removeAccent done

--- src/util/removeAccent.ts
+++ src/util/removeAccent.ts
@@ -2,21 +2,32 @@ const accents = 'ÀÁÂÃÄÅàáâãäåÒÓÔÕÕÖŐòóôõöőÈÉÊËèé�
 const noAccents = 'AAAAAAaaaaaaOOOOOOOooooooEEEEeeeeeCCccDggGGhhHHIIIIiiiijJkKlLmMnnNNUUUUUuuuuuNnpPrRssSSSstuuvVwwwwWWWWxXyyyyyyYYYYYzzzZZZ';
 
 const removeAccents = (text: string): string => {
-  const map = {};
-  accents.split('').forEach((el, idx) => map[el] = noAccents[idx]);
+  try {
+    const map = {};
+    accents.split('').forEach((el, idx) => map[el] = noAccents[idx]);
 
-  return text.replace(/[^A-Za-z0-9]/g, (ch) => map[ch] || ch);
+    return text.replace(/[^A-Za-z0-9]/g, (ch) => map[ch] || ch);
+  } catch (err) {
+    console.log('This function can handle just strings.\n' +
+                'Example: removeAccents("All your string here.")', err)
+  }
 }
 
 const removeAccentsArray = (textArray: string[]): string[] => {
-  const map = {};
-  accents.split('').forEach((el, idx) => map[el] = noAccents[idx])
+  try {
+    const map = {};
+    const newArray: string[] = []
+    accents.split('').forEach((el, idx) => map[el] = noAccents[idx])
 
-  for (const key in textArray) {
-    textArray[key].replace(/[^A-Za-z0-9]/g, (ch) => map[ch] || ch)
+    for (const key in textArray) {
+      newArray.push(textArray[key].replace(/[^A-Za-z0-9]/g, (ch) => map[ch] || ch))
+    }
+
+    return newArray;
+  } catch (err) {
+    console.log('This function can handle just string arrays.\n' +
+                'Example: removeAcentsArray(["First", "Second", "Third"])', err);
   }
-
-  return ;
 }
 
 export {
