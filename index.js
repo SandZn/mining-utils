811ca3c1fd090dd7831b17b6189c354c7b4a6744
Oracy Martos <oramartos_21@hotmail.com>
2019-10-25 18:28:54 -0300

    Remove Punctuation and blank spaces

--- index.js
+++ index.js
@@ -4,3 +4,4 @@ function __export(m) {
 }
 Object.defineProperty(exports, "__esModule", { value: true });
 __export(require("./src/util/removeAccent"));
+__export(require("./util/removePunctuation"));
