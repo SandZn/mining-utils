487c423d18300937b7d13ac8ac0e87d62fe513db
Lucas M Cardoso <clucasmedeiros@gmail.com>
2019-10-26 05:36:26 -0700

    Testing new build

---
+++ index.d.ts
@@ -0,0 +1,8 @@
+declare const removeAccents: (text: string) => string;
+declare const removeAccentsArray: (textArray: string[]) => string[];
+export { removeAccents, removeAccentsArray, };
+declare const removePunctuation: (text: string) => string;
+declare const removeBlankSpace: (text: string) => string;
+declare const removePunctuationArray: (textArray: string[]) => string[];
+declare const removeBlankSpaceArray: (textArray: string[]) => string[];
+export { removePunctuation, removeBlankSpace, removePunctuationArray, removeBlankSpaceArray };
